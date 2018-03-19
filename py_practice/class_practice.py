aa8dad060c38556bd66ae0b7204e2abe44636f16
kiya015 <452798806@qq.com>
2018-03-19 22:40:49 +0800

    home 3.19

--- py_practice/class_practice.py
+++ py_practice/class_practice.py
@@ -6,6 +6,24 @@ __author__ = 'kiya3-12'
 
 
 class Cat:
+    # 属性
 
+
+
+    # 方法
     def eat(self):
         print('猫在吃鱼====')
+
+    def drink(self):
+        print('猫在喝水。。')
+
+# 创建一个对象
+tom = Cat()
+
+# 调用tom指向对象中的方法
+tom.eat()
+tom.drink()
+
+# 给tom指向的对象添加属性
+tom.name = '汤姆'
+tom.age = 30
