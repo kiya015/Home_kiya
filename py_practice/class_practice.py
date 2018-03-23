9e6ba3b9ef647d4fd467cc71dc3805ee0ed0e805
kiya015 <452798806@qq.com>
2018-03-23 19:25:28 +0800

    company 3-19补交

--- py_practice/class_practice.py
+++ py_practice/class_practice.py
@@ -8,14 +8,15 @@ __author__ = 'kiya3-12'
 class Cat:
     # 属性
 
-
-
     # 方法
     def eat(self):
         print('猫在吃鱼====')
 
     def drink(self):
         print('猫在喝水。。')
+
+    def introduce(self):
+        print('%s的年纪是%s!' % (self.name, self.age))
 
 # 创建一个对象
 tom = Cat()
@@ -27,3 +28,15 @@ tom.drink()
 # 给tom指向的对象添加属性
 tom.name = '汤姆'
 tom.age = 30
+# 第一种实现属性的方法
+# print('%s的年纪是%d!'%(tom.name, tom.age))
+
+tom.introduce()
+
+blue = Cat()
+blue.eat()
+blue.drink()
+blue.name = '蓝猫'
+blue.age = '28'
+
+blue.introduce()
