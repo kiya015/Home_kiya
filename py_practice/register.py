6c6dbe0cd78357c0592dbd589ba5ad1c64f02a13
kiya015 <452798806@qq.com>
2018-03-23 19:12:42 +0800

    company3-23 去掉部分关键信息的内容

--- py_practice/register.py
+++ py_practice/register.py
@@ -2,20 +2,32 @@
 
 __author__ = 'kiya3-22'
 
-import http
+import requests
 import redis
+import time
 
 # 注册接口地址
 
-url = 'http://testeps.rabbitpre.com/api/user/loginqrcode'
 
+
+# 手机号
+phone = 13600175152
+# print(type(phone))
+code = 123452
+pwd = 'qweqwe'
 # 无法跳过图片验证码，但可以直接往redis插入数据实现注册流程
 
 # 连接redis
 
-pool = redis.ConnectionPool(host = '172.16.1.1',port = '443')
 
 r = redis.Redis(connection_pool=pool)
+
+
+
+d = requests.post(url, postData)
+print(d.content)
+
+print(d.json().get('message'))
 
 
 
