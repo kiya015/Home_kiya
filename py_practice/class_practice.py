3f2e2b227be8932db3436a4094c40e3836f2a285
kiya015 <452798806@qq.com>
2018-03-25 23:37:24 +0800

    home 3-25

--- py_practice/class_practice.py
+++ py_practice/class_practice.py
@@ -6,7 +6,16 @@ __author__ = 'kiya3-12'
 
 
 class Cat:
-    # 属性
+    '''定义一个Cat类'''
+    # 初始化对象
+    def __init__(self, newname, newage):
+        # print('-----test------')
+        self.name = newname
+        self.age = newage
+
+    # 对象的描述信息
+    def __str__(self):
+        return '%s的年纪是%s!' % (self.name, self.age)
 
     # 方法
     def eat(self):
@@ -19,24 +28,29 @@ class Cat:
         print('%s的年纪是%s!' % (self.name, self.age))
 
 # 创建一个对象
-tom = Cat()
-
+# tom = Cat()
+tom = Cat('汤姆',33)
 # 调用tom指向对象中的方法
-tom.eat()
-tom.drink()
+# tom.eat()
+# tom.drink()
 
 # 给tom指向的对象添加属性
-tom.name = '汤姆'
-tom.age = 30
+# tom.name = '汤姆'
+# tom.age = 30
 # 第一种实现属性的方法
 # print('%s的年纪是%d!'%(tom.name, tom.age))
 
-tom.introduce()
+# tom.introduce()
 
-blue = Cat()
-blue.eat()
-blue.drink()
-blue.name = '蓝猫'
-blue.age = '28'
+# blue = Cat()
+blue = Cat('蓝猫',20)
+# blue.eat()
+# blue.drink()
+# blue.name = '蓝猫'
+# blue.age = '28'
 
-blue.introduce()
+# blue.introduce()
+
+
+print(tom)
+print(blue)
