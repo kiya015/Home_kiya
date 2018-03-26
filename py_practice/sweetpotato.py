97a2a38cd04fca9483df55d1e66ef84020d10fbc
kiya015 <452798806@qq.com>
2018-03-26 20:27:59 +0800

    检查出错误

--- py_practice/sweetpotato.py
+++ py_practice/sweetpotato.py
@@ -4,37 +4,45 @@ __author__ = 'kiya3-26'
 
 # 用类实现烤地瓜应用
 
-class SweetPotato:
+class potato:
 
-	def __int__(self):
-		self.cookedstring = '生的'
-		self.cook_time = 0
+	def __init__(self):   # 此处因为init写为int导致错误一直未发现并修正
+		self.cookeda = '生的'
+		self.cooklevel = 0
+		self.condiments = []  # 多个属性
 
 	def __str__(self):
-		return "地瓜状态：%s(%d)"%(self.cookedstring,self.cook_time)
+		return '地瓜状态：%s(%d)，添加佐料有：%s'%(self.cookeda, self.cooklevel, str(self.condiments))
 
 	def cook(self, time):
 
-		self.cook_time += time
+		self.cooklevel += time
+		if self.cooklevel>0  and  self.cooklevel<= 3:
+			self.cookeda = '还是生的呢'
 
-		if 0< self.cook_time and self.cook_time<= 3:
-			self.cookedstring = '还是生的呢'
+		elif self.cooklevel>3  and self.cooklevel <=5:
 
-		elif  3<self.cook_time  and self.cook_time <= 5:
-			self.cookedstring = '还需要加油再烤烤哟'
+			self.cookeda = '还需要加油再烤烤哟'
 
-		elif  5< self.cook_time and self.cook_time < 8:
-			self.cookedstring = '刚刚好，可以开吃啦。。'
+		elif self.cooklevel>5 and self.cooklevel <8:
+			self.cookeda = '刚刚好，可以开吃啦。。'
 
-		elif  self.cook_time >= 8:
-			self.cookedstring = '烤焦啦。。。'
+		elif self.cooklevel >= 8:
+			self.cookeda = '烤焦啦。。。'
+
+	def addCondinments(self,item):
+		self.condiments.append(item)
+
 
 
 
 # 创建对象(实例化)
-gua = SweetPotato()
-print(gua)
 
+gua = potato()
 
-gua.cook(1)
 # print(gua)
+
+
+gua.cook(5)
+gua.addCondinments('辣椒')
+print(gua)
