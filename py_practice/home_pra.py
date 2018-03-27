8a33afcaf3aef0907b4e7c5fc0db7b5b205fa7e5
kiya015 <452798806@qq.com>
2018-03-27 20:39:50 +0800

    company class应用3-27

--- py_practice/home_pra.py
+++ py_practice/home_pra.py
@@ -12,11 +12,13 @@ class Home:
 		self.contain_item = []
 
 	def __str__(self):
-		return  '房子的总面积是：%d,地址：%s,户型：%s,放入%s后的面积是%d' % (self.area, self.info, self.addr, self.contain_item, self.nnarea)
+		return  '房子的总面积是：%d,地址：%s,户型：%s,放入%s后的面积是:%d' % (self.area, self.info, self.addr, self.contain_item, self.nnarea)
 
 	def add_item(self, item):
-		self.nnarea -= item.area
-		self.contain_item.append(item.info)
+		# self.nnarea -= item.area
+		# self.contain_item.append(item.info)
+		self.nnarea -= item.get_area()
+		self.contain_item.append(item.get_info())
 
 class Bed:
 	def __init__(self, ninfo, narea):
@@ -26,6 +28,12 @@ class Bed:
 	def __str__(self):
 		return '%s占用面积是：%d' % (self.info, self.area)
 
+	def get_area(self):
+		return  self.area
+
+	def get_info(self):
+		return self.info
+
 # 创建个对象，Home实例化
 
 jia = Home(150, '深圳南山', '三室两厅')
@@ -34,7 +42,7 @@ bed1 = Bed('席梦思', 5)
 
 jia.add_item(bed1)
 
-bed2 = Bed('大圆床', 10)
+bed2 = Bed('大圆床', 102)
 jia.add_item(bed2)
 
 print(jia)
