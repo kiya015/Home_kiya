7e388fc0f6e5577d6c22428f78e865fb508bae75
kiya015 <452798806@qq.com>
2018-04-03 22:49:36 +0800

    Home4-3

---
+++ py_practice/__del__方法.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+__author__ = 'kiya4-3'
+
+class Dog:
+
+    def __del__(self):
+        print("----英雄over-------")
+
+
+
+
+dog1 = Dog()
+dog2 = dog1
+del dog1   # 不会调用__del__方法，因为这个对象还有其它变量指向它
+del dog2   # 此时会调用__del__方法，因为没有变量指向它了
+print('=========')
+
+
+# 如果在程序结束时，有些对象还存在，那个python解释器会自动调用它们的__del__方法来完成清理工作
+
+del dog2
