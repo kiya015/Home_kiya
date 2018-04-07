df49a88baf3572bfe1de080b07d5fa5d4cc65ba8
kiya <452798806@qq.com>
2018-04-07 21:38:43 +0800

    Home04-7

--- py_practice/类属性、实例属性.py
+++ py_practice/类属性、实例属性.py
@@ -2,12 +2,17 @@
 
 __author__ = 'kiya04-7'
 
+# 理解类对象  实际类也是一个对象，对象名是Tools
 class Tools(object):
     # 属性
-    num = 0
+    num = 0  # 类属性
     # 方法
     def __init__(self, new_name):
-        self.name = new_name
+        self.name = new_name  # 实例属性
+        Tools.num += 1   # 调用类属性
 
-
+# 实例化对象，实际有实例属性及方法
 tool_1 = Tools('铁锹')
+tool_2 = Tools('铁锹')
+tool_3 = Tools('铁锹')
+print(Tools.num)
