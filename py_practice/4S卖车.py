dc8aadcb5a1806aade3b2cd3bb1b45c575e45c30
kiya <452798806@qq.com>
2018-04-10 22:10:46 +0800

    Home04-10

--- py_practice/4S卖车.py
+++ py_practice/4S卖车.py
@@ -7,9 +7,28 @@ class CarStore(object):
 	# def order(self, money):
 	# 	if money > 50000:
 	# 		return  Car()
+	# def order(self, car_type):
+	# 	if car_type == "现代":
+	# 		return  Xd_car()
+	def __init__(self):
+		self.factorys = Factory() # 此处未完全理解
+
 	def order(self, car_type):
+		return self.factorys.factory(car_type)  # 此处未完全理解
+
+	# def order(self, car_type):
+	# 	return factory(car_type)
+
+class Factory(object):
+	def factory(self, car_type):
 		if car_type == "现代":
-			return  Xd_car()
+			return Xd_car()
+		elif car_type == "别克":
+			return Bk_car()
+		# else:
+		# 	print('无此车型')
+
+
 
 class Car(object):
 	def move(self):
@@ -19,6 +38,9 @@ class Car(object):
 		print('停车---')
 
 class Xd_car(Car):
+	pass
+
+class Bk_car(Car):
 	pass
 
 
